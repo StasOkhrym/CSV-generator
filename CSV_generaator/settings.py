28d51de2897c6a99da9fd608b720772197b51267
Stas Okhrym <s.okhrym@gmail.com>
2022-12-09 16:38:48 +0200

    Added urls for app

--- CSV_generaator/settings.py
+++ CSV_generaator/settings.py
@@ -100,6 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "csv-generator.User"
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
